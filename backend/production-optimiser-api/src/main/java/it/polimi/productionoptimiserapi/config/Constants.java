266e670e4eed9fa3b07e1353f5a459bbc94a25ab
Giovanni Orciuolo <giovanni.orciuolo1999@gmail.com>
2024-11-04 17:46:02 +0100

    chore(formatting): Add Java Spotless with Google codestyle, fix existing files

--- backend/production-optimiser-api/src/main/java/it/polimi/productionoptimiserapi/config/Constants.java
+++ backend/production-optimiser-api/src/main/java/it/polimi/productionoptimiserapi/config/Constants.java
@@ -2,6 +2,5 @@ package it.polimi.productionoptimiserapi.config;
 
 public class Constants {
 
-    public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
-
+  public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 }
--- backend/production-optimiser-api/src/main/java/it/polimi/productionoptimiserapi/entities/User.java
+++ backend/production-optimiser-api/src/main/java/it/polimi/productionoptimiserapi/entities/User.java
@@ -4,15 +4,14 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import it.polimi.productionoptimiserapi.enums.UserRole;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.springframework.security.crypto.bcrypt.BCrypt;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -23,40 +22,36 @@ import java.util.Set;
 @Setter
 public class User extends BaseEntity {
 
-    @Column(unique = true)
-    @NotNull
-    private String email;
+  @Column(unique = true)
+  @NotNull
+  private String email;
 
-    @Column
-    @NotNull
-    private String password;
+  @Column @NotNull private String password;
 
-    @Column
-    @NotNull
-    @Enumerated(EnumType.STRING)
-    private UserRole role = UserRole.CUSTOMER;
+  @Column
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  private UserRole role = UserRole.CUSTOMER;
 
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(
-        name = "users_optimization_models",
-        joinColumns = @JoinColumn(name = "user_id"),
-        inverseJoinColumns = @JoinColumn(name = "optimization_model_id" )
-    )
-    @JsonManagedReference
-    private Set<OptimizationModel> availableOptimizationModels = new HashSet<>();
+  @ManyToMany(cascade = CascadeType.ALL)
+  @JoinTable(
+      name = "users_optimization_models",
+      joinColumns = @JoinColumn(name = "user_id"),
+      inverseJoinColumns = @JoinColumn(name = "optimization_model_id"))
+  @JsonManagedReference
+  private Set<OptimizationModel> availableOptimizationModels = new HashSet<>();
 
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
-    @JsonManagedReference
-    private Set<OptimizationResult> optimizationResults = new HashSet<>();
+  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+  @JsonManagedReference
+  private Set<OptimizationResult> optimizationResults = new HashSet<>();
 
-    @PrePersist
-    @PreUpdate
-    public void onPersistAndUpdate() {
-        this.hashPassword();
-    }
+  @PrePersist
+  @PreUpdate
+  public void onPersistAndUpdate() {
+    this.hashPassword();
+  }
 
-    private void hashPassword() {
-        this.password = BCrypt.hashpw(this.password, BCrypt.gensalt(12));
-    }
-
+  private void hashPassword() {
+    this.password = BCrypt.hashpw(this.password, BCrypt.gensalt(12));
+  }
 }
