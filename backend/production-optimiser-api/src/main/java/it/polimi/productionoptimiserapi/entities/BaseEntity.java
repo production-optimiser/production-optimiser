ca8b4ab6a963567139903acb226ecad636118088
Theanko <danko.curlin@hotmail.com>
2024-11-09 22:41:30 +0100

    fix

--- backend/production-optimiser-api/src/main/java/it/polimi/productionoptimiserapi/entities/BaseEntity.java
+++ backend/production-optimiser-api/src/main/java/it/polimi/productionoptimiserapi/entities/BaseEntity.java
@@ -1,7 +1,11 @@
 package it.polimi.productionoptimiserapi.entities;
 
+import static it.polimi.productionoptimiserapi.config.Constants.DATETIME_FORMAT;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +13,6 @@ import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
-import static it.polimi.productionoptimiserapi.config.Constants.DATETIME_FORMAT;
 @MappedSuperclass
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,18 +20,17 @@ import static it.polimi.productionoptimiserapi.config.Constants.DATETIME_FORMAT;
 @Data
 public abstract class BaseEntity implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
 
-    @Column(name = "created_at")
-    @CreationTimestamp
-    @JsonFormat(pattern=DATETIME_FORMAT)
-    private LocalDateTime createdAt;
+  @Column(name = "created_at")
+  @CreationTimestamp
+  @JsonFormat(pattern = DATETIME_FORMAT)
+  private LocalDateTime createdAt;
 
-    @Column(name = "updated_at")
-    @UpdateTimestamp
-    @JsonFormat(pattern=DATETIME_FORMAT)
-    private LocalDateTime updatedAt;
-
+  @Column(name = "updated_at")
+  @UpdateTimestamp
+  @JsonFormat(pattern = DATETIME_FORMAT)
+  private LocalDateTime updatedAt;
 }
