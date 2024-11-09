25cf8b78fe47b2abbc71e3c6c84230567d6168c7
Theanko <danko.curlin@hotmail.com>
2024-11-09 23:04:21 +0100

    fix

--- backend/production-optimiser-api/src/test/java/it/polimi/productionoptimiserapi/ProductionOptimiserApiApplicationTests.java
+++ backend/production-optimiser-api/src/test/java/it/polimi/productionoptimiserapi/ProductionOptimiserApiApplicationTests.java
@@ -1,13 +1,10 @@
 package it.polimi.productionoptimiserapi;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProductionOptimiserApiApplicationTests {
 
-  @Test
-  @Disabled
+  //  @Test
   void contextLoads() {}
 }
