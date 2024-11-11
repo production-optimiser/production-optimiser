c354793b1d355dadfeb60d375097f80e7001374c
Paula <paula.salkovic@fer.hr>
2024-11-11 03:45:54 +0100

    mvn spotless apply

--- backend/production-optimiser-api/src/main/java/it/polimi/productionoptimiserapi/repositories/UserRepository.java
+++ backend/production-optimiser-api/src/main/java/it/polimi/productionoptimiserapi/repositories/UserRepository.java
@@ -1,9 +1,8 @@
 package it.polimi.productionoptimiserapi.repositories;
 
 import it.polimi.productionoptimiserapi.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByEmail(String email);
