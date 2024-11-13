3e3703e57bc0e786aaf71af7c212bc48f518ad11
Giovanni Orciuolo <giovanni.orciuolo1999@gmail.com>
2024-11-13 20:01:03 +0100

    feat: OptimizationModel controller/service/repository

--- backend/production-optimiser-api/src/main/java/it/polimi/productionoptimiserapi/entities/BaseEntity.java
+++ backend/production-optimiser-api/src/main/java/it/polimi/productionoptimiserapi/entities/BaseEntity.java
@@ -1,23 +1,24 @@
 package it.polimi.productionoptimiserapi.entities;
 
-import static it.polimi.productionoptimiserapi.config.Constants.DATETIME_FORMAT;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import java.io.Serializable;
-import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import static it.polimi.productionoptimiserapi.config.Constants.DATETIME_FORMAT;
 
 @MappedSuperclass
 @AllArgsConstructor
 @NoArgsConstructor
-@SuperBuilder
-@Data
+@Getter
+@Setter
+@ToString
 public abstract class BaseEntity implements Serializable {
 
   @Id
@@ -33,4 +34,20 @@ public abstract class BaseEntity implements Serializable {
   @UpdateTimestamp
   @JsonFormat(pattern = DATETIME_FORMAT)
   private LocalDateTime updatedAt;
+
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null) return false;
+    Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+    Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+    if (thisEffectiveClass != oEffectiveClass) return false;
+    BaseEntity that = (BaseEntity) o;
+    return getId() != null && Objects.equals(getId(), that.getId());
+  }
+
+  @Override
+  public final int hashCode() {
+    return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+  }
 }
---
+++ backend/production-optimiser-api/src/main/java/it/polimi/productionoptimiserapi/repositories/OptimizationModelRepository.java
@@ -0,0 +1,7 @@
+package it.polimi.productionoptimiserapi.repositories;
+
+import it.polimi.productionoptimiserapi.entities.OptimizationModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptimizationModelRepository extends JpaRepository<OptimizationModel, Long> {
+}
