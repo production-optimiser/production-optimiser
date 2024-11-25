4fb1b225b200b6f88a1a5a23c3d6ed8ce75cbe76
shreeshjha <shreeshkumar2@gmail.com>
2024-11-25 11:26:29 +0100

    Fix: Integration for Login

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 // vite.config.js
-import { defineConfig } from 'vite';
+/*import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-
 export default defineConfig({
   base: 'https://production-optimiser.github.io/production-optimiser/',
   plugins: [react()],
@@ -16,4 +15,35 @@ export default defineConfig({
       overlay: false
     }
   }
+});*/ 
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      hmr: {
+        overlay: false
+      }
+    }
+  };
+
+  if (command === 'serve') {
+    // Development-specific config
+    config.base = '/';
+  } else {
+    // Production-specific config
+    config.base = '/production-optimiser/';
+  }
+
+  return config;
 });
